caff8d5b0edc63ceffb0b7d0bd4eedca787fdaa9
LeandroTorresSicilia <jtorressicilia@gmail.com>
2020-01-11 19:22:07 -0600

    fix: export createAddDocAction

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as FirebaseProvider } from './firebase-provider';
 export { default as useCollectionOnce } from './firestore/useCollectionOnce';
 export { default as useCollection } from './firestore/useCollection';
+export { default as createAddDocAction } from './firestore/createAddDocAction';
 export { default as useDoc } from './firestore/useDoc';
