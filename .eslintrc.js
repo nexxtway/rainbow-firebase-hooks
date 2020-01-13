f0068b850eb61f39c59df8eb1730dbcc85032ffd
Maxx Greene <maxx@nexxtway.com>
2020-01-12 23:33:35 -0500

    feat: PoC of run jest tests with firebase emulators

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
-    extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+        'plugin:react/recommended',
+    ],
     parserOptions: {
         ecmaVersion: 2018,
         sourceType: 'module',
---
+++ scripts/run-jest-tests.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+const jest = require('jest');
+jest.run('./src --verbose --passWithNoTests');
